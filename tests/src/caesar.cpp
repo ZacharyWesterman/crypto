f455bfce2b03fc3f26e9f937289b25f645e5a9c8
Joe Shoulak <joepshoulak@me.com>
2024-09-19 17:40:10 -0500

    polish

--- tests/src/caesar.cpp
+++ tests/src/caesar.cpp
@@ -2,31 +2,31 @@
 
 #include "../../ciphers/caesar.cpp"
 
-zstring caesarInputA = "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
-zstring caesarOutputA = "Sp ro rkn kxidrsxq myxpsnoxdskv dy cki, ro gbydo sd sx mszrob, drkd sc, li cy mrkxqsxq dro ybnob yp dro voddobc yp dro kvzrklod, drkd xyd k gybn myevn lo wkno yed.";
+zstring caesarInput = "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
+zstring caesarOutput = "Sp ro rkn kxidrsxq myxpsnoxdskv dy cki, ro gbydo sd sx mszrob, drkd sc, li cy mrkxqsxq dro ybnob yp dro voddobc yp dro kvzrklod, drkd xyd k gybn myevn lo wkno yed.";
 
-zstring caesarInputB = "Ifhehadanythingconfidentialtosayhewroteitincipherthatisbysochangingtheorderofthelettersofthealphabetthatnotawordcouldbemadeout";
-zstring caesarOutputB = "Sprorknkxidrsxqmyxpsnoxdskvdyckirogbydosdsxmszrobdrkdsclicymrkxqsxqdroybnobypdrovoddobcypdrokvzrkloddrkdxydkgybnmyevnlowknoyed";
+zstring caesarInputCharsOnly = "Ifhehadanythingconfidentialtosayhewroteitincipherthatisbysochangingtheorderofthelettersofthealphabetthatnotawordcouldbemadeout";
+zstring caesarOutputCharsOnly = "Sprorknkxidrsxqmyxpsnoxdskvdyckirogbydosdsxmszrobdrkdsclicymrkxqsxqdroybnobypdrovoddobcypdrokvzrkloddrkdxydkgybnmyevnlowknoyed";
 
 TEST_CASE("Testing Caesar Encode")
 {
-  REQUIRE(caesarEncode(caesarInputA, 10) == caesarOutputA);
-  REQUIRE(caesarEncode(caesarInputB, 10) == caesarOutputB);
+  REQUIRE(caesarEncode(caesarInput, 10) == caesarOutput);
+  REQUIRE(caesarEncode(caesarInputCharsOnly, 10) == caesarOutputCharsOnly);
 }
 
 TEST_CASE("Testing Caesar Decode")
 {
-  REQUIRE(caesarDecode(caesarOutputA, 10) == caesarInputA);
-  REQUIRE(caesarDecode(caesarOutputB, 10) == caesarInputB);
+  REQUIRE(caesarDecode(caesarOutput, 10) == caesarInput);
+  REQUIRE(caesarDecode(caesarOutputCharsOnly, 10) == caesarInputCharsOnly);
 }
 
 TEST_CASE("Testing Caesar Reciprocity")
 {
-  REQUIRE(caesarDecode(caesarEncode(caesarInputA, 10), 10) == caesarInputA);
-  REQUIRE(caesarDecode(caesarEncode(caesarInputB, 10), 10) == caesarInputB);
+  REQUIRE(caesarDecode(caesarEncode(caesarInput, 10), 10) == caesarInput);
+  REQUIRE(caesarDecode(caesarEncode(caesarInputCharsOnly, 10), 10) == caesarInputCharsOnly);
 }
 
 TEST_CASE("Testing Caesar Crack")
 {
-  REQUIRE(caesarCrack(caesarOutputA)[0].text == caesarInputA);
+  REQUIRE(caesarCrack(caesarOutput)[0].text == caesarInput);
 }
