14e20431d284e9db6af1f729a9877061ccef32a8
Joe Shoulak <joepshoulak@me.com>
2024-09-30 18:48:49 -0500

    polish

--- tools/resultViewer.py
+++ tools/resultViewer.py
@@ -15,7 +15,7 @@ for line in stdin:
 print("Displaying data... ", end='', flush=True)
 
 plt.bar(list(scores.keys()), [i/total for i in list(scores.values())])
-plt.title("Word Search Score Distribution")
+plt.title("Result Viewer")
 plt.get_current_fig_manager().set_window_title("Word Search Score Distribution")
 plt.show()
 
--- tools/wordSearchEvaluator.cpp
+++ tools/wordSearchEvaluator.cpp
@@ -9,17 +9,14 @@ using namespace std;
 void deleteMessage(zstring message, ostream &os)
 {
   for (int i : z::core::range(message.length()))
-  {
-    os << '\b'; // FIXME
-  }
+    os << '\b' << ' ' << '\b' << flush;
 }
 
 int main(int argc, char *argv[])
 {
   zstring().writeln(cerr);
 
-  // for (auto id : z::core::array<zstring>{"1", "2", "3", "4"})
-  for (auto id : z::core::array<zstring>{"4"})
+  for (auto id : z::core::array<zstring>{"1", "2", "3", "4"})
   {
     zstring message;
     auto lines = z::file::lines("data/p"_zs + id + ".txt").collect();
@@ -40,7 +37,7 @@ int main(int argc, char *argv[])
         if (!p.count('.'))
           p += ".0";
 
-        message = "["_zs + p + "%]";
+        message = p + "%";
         message.write(cerr);
       }
 
@@ -49,7 +46,7 @@ int main(int argc, char *argv[])
 
     deleteMessage(message, cerr);
 
-    cerr << "Done!   " << endl;
+    cerr << "Done!" << endl;
 
     z::core::join(result, ',').writeln(cout);
   }
