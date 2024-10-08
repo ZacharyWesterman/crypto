2197768ea2ca5dc07b366ff87ad56245f59fce67
Joe Shoulak <joepshoulak@me.com>
2024-10-07 21:58:03 -0500

    loading bar from libzed now

--- tools/analyzer/analyzeHelper.h
+++ tools/analyzer/analyzeHelper.h
@@ -7,76 +7,8 @@ using namespace std;
 
 // TODO: Split wiki into wordSearch and spellCheck
 
-struct LoadingBar
-{
-  int length;
-  bool showPercent;
-  ostream &os;
-  zstring message;
-
-  LoadingBar(int _length = 102, ostream &_os = cout, bool _showPercent = true) : length(_length - 2), showPercent(_showPercent), os(_os), message("") {};
-
-  void clear()
-  {
-    for (int i : z::core::range(message.length()))
-      os << "\b \b" << flush;
-  }
-
-  zstring getBar(float progress)
-  {
-    zstring bar = "[";
-
-    for (int i : z::core::range(length))
-      bar += i < progress * length ? "#" : " ";
-
-    return bar + "]";
-  }
-
-  zstring getPercentage(float progress)
-  {
-    zstring p;
-
-    if (progress == 1)
-    {
-      p += "Done!";
-    }
-    else
-    {
-      p += zstring(((int)(progress * 1000)) / 10.0);
-
-      if (!p.count('.'))
-        p += ".0";
-
-      p += "%";
-    }
-
-    return p;
-  }
-
-  void update(float progress = 1)
-  {
-    clear();
-
-    message = getBar(progress);
-
-    if (showPercent)
-      message += " "_zs + getPercentage(progress);
-
-    message.write(os);
-  }
-
-  void finish()
-  {
-    update();
-    message = "";
-    os << endl;
-  }
-};
-
 void analyze(std::function<float(zstring)> evaluator, bool random = false)
 {
-  LoadingBar loadingBar(102, cerr);
-
 #define TESTING
   if (random)
   {
@@ -89,15 +21,12 @@ void analyze(std::function<float(zstring)> evaluator, bool random = false)
 
     ("  Generating "_zs + total + " random paragraphs... ").write(cerr);
 
-    for (int i : z::core::range(total)) // roughly the number of paragraphs
+    z::util::progress loadingBar;
+    for (int i : z::core::range(total))
     {
       result.push(evaluator(""));
-
-      if (i % 50 == 0)
-        loadingBar.update(i / total);
+      loadingBar.set(std::cerr, i, total);
     }
-
-    loadingBar.finish();
 
     z::core::join(result, ',').writeln(cout);
 
@@ -118,17 +47,13 @@ void analyze(std::function<float(zstring)> evaluator, bool random = false)
     int i = 0;
     z::core::array<zstring> result;
 
+    z::util::progress loadingBar;
     for (auto line : lines)
     {
       result.push(evaluator(line.trim()));
-
-      if (i % 50 == 0)
-        loadingBar.update(i / total);
-
+      loadingBar.set(std::cerr, i, total);
       i++;
     }
-
-    loadingBar.finish();
 
     z::core::join(result, ',').writeln(cout);
   }
